d113dd42d98686ebcec9c9fa9f5997e5c839d010
root <root@ubuntu>
2014-12-15 12:39:22 +0100

    smart way of waiting before autorun

--- conf/udev/scripts/usb_autorun.sh
+++ conf/udev/scripts/usb_autorun.sh
@@ -2,9 +2,21 @@
 
 SCRIPT=autoexec.sh
 
+echo > /tmp/autorun
+
 autorun() {
+    # wait until the system has boot (no rc scripts running up to 30 seconds)
+    for i in `seq 1 20` ; do
+        XX="`ps aux`" ; if  ! ( echo $XX | grep "rc " ) ; then 
+		echo "AUTORUN BREAK" >> /tmp/autorun
+		break;
+	else
+		sleep 1
+		echo "Waiting system boot complete before starting autorun.sh script $1 ..." | logger -t "AUTORUN"
+	fi
+    done
+
     FILE=/mnt/${1}/${SCRIPT}
-    sleep 10
 
     if [ -f ${FILE} ]; then
         /bin/bash ${FILE}
