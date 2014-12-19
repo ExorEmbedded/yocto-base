093ae842937e3314382eb18837b6609d86951a1c
Steven Dorigotti <s.dorigotti@endian.com>
2014-12-19 22:44:15 +0100

    conf/X11/Xsession.d: replace Demo with JMUConfigApp launch

--- conf/X11/Xsession.d/80xDemo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-DISPLAY=:0 exec /usr/bin/xterm -e "/sbin/ifconfig ; /bin/sh" &
---
+++ conf/X11/Xsession.d/80xJMUConfigApp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+JMUCONFIG_TESTURL="localhost:3000/api/v1/system"
+
+jmuconfig_wait()
+{
+    while true; do
+        wget -O - "${JMUCONFIG_TESTURL}" >/dev/null 2>&1
+        [ $? -eq 0 ] && break
+        logger -t $0 "waiting for JMUConfig"
+        sleep 1
+    done
+}
+
+{
+    jmuconfig_wait
+
+    logger -t $0 "Running jmuconfig-app"
+
+    DISPLAY=:0 exec jmuconfig-app 2>&1 | logger -t JMUConfig-app
+
+} &
