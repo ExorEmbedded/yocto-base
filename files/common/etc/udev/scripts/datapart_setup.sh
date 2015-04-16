7a44839413fc5db34693902cbef2a7865dd643ab
Steven Dorigotti <stewy@koanlogic.com>
2015-04-16 19:31:49 +0200

    udev: datapart_setup.sh applied to wrong partition since also /home mountpoint exists on /dev/mmcblk1p6

--- files/common/etc/udev/scripts/datapart_setup.sh
+++ files/common/etc/udev/scripts/datapart_setup.sh
@@ -3,6 +3,8 @@
 # Activate setgid bit on data partition mountpoint so directories and files
 # inherit group permissions
 
-MNTNAME=$(grep ${DEVNAME} /proc/mounts | awk '{print $2}')
+[ "${DEVNAME}" != "/dev/mmcblk1p6" ] && exit 0
+
+MNTNAME=/mnt/data
 chgrp data "${MNTNAME}"
 chmod 2774 "${MNTNAME}"  # setgid + rwx for group
