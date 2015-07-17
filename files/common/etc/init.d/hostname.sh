f602a4e0df5fc9ba5e416ef631b8014c10700fd9
Steven Dorigotti <stewy@koanlogic.com>
2015-07-17 09:58:16 +0200

    init: set hostname using exorint_hostname (if no /etc/hostname found)

---
+++ files/common/etc/init.d/hostname.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          hostname
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Set hostname based on BSP version
+### END INIT INFO
+
+. /etc/exorint.funcs
+
+if [ ! -r /etc/hostname ]; then
+    hn="$(exorint_hostname)"
+    if [ $? -ne 0 ]; then
+        hostname usom
+    else
+        hostname ${hn}
+    fi
+else
+    hostname -F /etc/hostname
+fi
