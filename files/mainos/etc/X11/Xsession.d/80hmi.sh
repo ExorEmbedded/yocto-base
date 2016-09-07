2a91fc491cb3527543dd952ca9edbfb32c0b9a4f
Luigi Scagnet <luigi.scagnet@exorint.it>
2016-09-07 14:39:18 +0200

    Risolto problema Blank screen, aggiunta opzione xeyes

--- files/mainos/etc/X11/Xsession.d/80hmi.sh
+++ files/mainos/etc/X11/Xsession.d/80hmi.sh
@@ -9,25 +9,35 @@ ln -s /proc/self/fd/0   			/dev/stdin
 ln -s /proc/self/fd     			/dev/fd
 ln -s /sys/bus/spi/devices/spi1.0/eeprom 	/dev/fram
 
-if [[ -e /mnt/data ]] ; then
-	# prepare foldef for jmlauncher
-	if [[ ! -e /mnt/data/hmi ]] ; then
-		mkdir /mnt/data/hmi 2> /dev/null
-	fi
-fi
+#ONLY for debug
+XEYES=y
 
-if [[ ! -e /etc/jmlauncher/ ]] ; then
-	mkdir /etc/jmlauncher
-fi
-
-
-if [[ -e /etc/nokiosk ]] ; then
-	echo "HMI: NOKIOSK" | logger
-	# starts the HMI
-	rm /etc/nokiosk # next boot will reset to default kiosk mode
-	DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchDesktop
+if [ $XEYES == "y" ]
+then
+    DISPLAY=:0 xeyes
 else
-	echo "HMI: KIOSK" | logger
-	# starts the desktop
-	DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchHMI | logger
+
+	if [[ -e /mnt/data ]] ; then
+		# prepare foldef for jmlauncher
+		if [[ ! -e /mnt/data/hmi ]] ; then
+			mkdir /mnt/data/hmi 2> /dev/null
+		fi
+	fi
+
+	if [[ ! -e /etc/jmlauncher/ ]] ; then
+		mkdir /etc/jmlauncher
+	fi
+
+
+	if [[ -e /etc/nokiosk ]] ; then
+		echo "HMI: NOKIOSK" | logger
+		# starts the HMI
+		rm /etc/nokiosk # next boot will reset to default kiosk mode
+		DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchDesktop
+	else
+		echo "HMI: KIOSK" | logger
+		# starts the desktop
+		DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchHMI | logger
+	fi
+
 fi
