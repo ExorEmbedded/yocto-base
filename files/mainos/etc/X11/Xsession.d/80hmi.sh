3f133e2047626b22821c2786d6c7f525a51b36bb
Luigi Scagnet <luigi.scagnet@exorint.it>
2017-04-05 17:19:40 +0200

    BSP-574 removed etc/X11/Xsession_FastBoot.d. Fastboot env is now setted in standard/etc/X11/Xsession.d/80hmi.sh

--- files/mainos/etc/X11/Xsession.d/80hmi.sh
+++ files/mainos/etc/X11/Xsession.d/80hmi.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-ln -s /dev/input/event0 			/dev/input/beeper
-ln -s /dev/input/event2 			/dev/input/touchscreen0
 ln -s /dev/rtc0         			/dev/rtc
 ln -s /proc/self/fd/2   			/dev/stderr
 ln -s /proc/self/fd/1   			/dev/stdout
 ln -s /proc/self/fd/0   			/dev/stdin
 ln -s /proc/self/fd     			/dev/fd
-ln -s /sys/bus/spi/devices/spi1.0/eeprom 	/dev/fram
 
+# Source defaults.
+. /etc/default/rcS
 
 if [[ -e /mnt/data ]] ; then
 	# prepare foldef for jmlauncher
@@ -21,15 +20,21 @@ if [[ ! -e /etc/jmlauncher/ ]] ; then
 	mkdir /etc/jmlauncher
 fi
 
-
 if [[ -e /etc/nokiosk ]] ; then
 	echo "HMI: NOKIOSK" | logger
 	# starts the HMI
 	rm /etc/nokiosk # next boot will reset to default kiosk mode
 	DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchDesktop
 else
+    if [ ! -z "$FASTBOOT" ] && [ -e "/mnt/data/hmi/$FASTBOOT/run.sh" ]; then
+	echo "HMI: KIOSK - FASTBOOT" | logger
+	cd /mnt/data/hmi/$FASTBOOT
+	./run.sh &
+	sleep 20;
+    else
 	echo "HMI: KIOSK" | logger
 	# starts the desktop
 	DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchHMI | logger
+    fi
 fi
 
