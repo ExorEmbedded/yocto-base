fa305b4de53963c9e5388aa40c6a07875c1c1c59
Luigi Scagnet <luigi.scagnet@exorint.it>
2017-05-02 17:11:00 +0200

    #574 Better parsing of jmlauncher.xml.

--- files/mainos/etc/X11/Xsession.d/80hmi.sh
+++ files/mainos/etc/X11/Xsession.d/80hmi.sh
@@ -9,11 +9,23 @@ ln -s /proc/self/fd     			/dev/fd
 # Source defaults.
 . /etc/default/rcS
 
-
-if [ -e "/mnt/data/hmi/$FASTBOOT/run.sh" ]; then
-    app_to_launch=$(grep autostart /mnt/data/hmi/jmlauncher.xml | grep -c 1)
+# Fast parse of jmlauncher.xml
+if [ ! -z "$FASTBOOT" ] && [ -e "/mnt/data/hmi/jmlauncher.xml" ]
+then
+    apps_to_launch=$(grep autostart /mnt/data/hmi/jmlauncher.xml | grep -c 1)
+    if [ "$apps_to_launch" = "1" ]
+    then
+        app_to_launch_name=$(grep autostart /mnt/data/hmi/jmlauncher.xml -C 2 | grep installationFolder | awk -F '[<>]' '{print $3}');
+        if [ "$app_to_launch_name" != "$FASTBOOT" ]
+        then
+            #Change FASTBOOT
+            sed -i "s/^\(FASTBOOT\s*=\s*\).*\$/\1$app_to_launch_name;/" /etc/default/rcS
+            #Faster then command: source /etc/default/rcS
+            FASTBOOT=$app_to_launch_name;
+        fi
+    fi
 else
-    app_to_launch=0;
+    apps_to_launch=0;
 fi
 
 if [[ -e /mnt/data ]] ; then
@@ -33,9 +45,9 @@ if [[ -e /etc/nokiosk ]] ; then
 	rm /etc/nokiosk # next boot will reset to default kiosk mode
 	DISPLAY=:0 dbus-send --system --print-reply --dest=com.exor.JMLauncher '/' com.exor.JMLauncher.launchDesktop
 else
-    if [ ! -z "$FASTBOOT" ] && [ -e "/mnt/data/hmi/$FASTBOOT/run.sh" ] && [ "$app_to_launch" -eq "1" ]; then
+    if [ ! -z "$FASTBOOT" ] && [ -e "/mnt/data/hmi/$FASTBOOT/run.sh" ] && [ "$apps_to_launch" -eq "1" ]; then
         echo "HMI: KIOSK - FASTBOOT" | logger
-        cd /mnt/data/hmi/$FASTBOOT
+        cd /mnt/data/hmi/"$FASTBOOT" || return
         ./run.sh &
         sleep 20;
     else
