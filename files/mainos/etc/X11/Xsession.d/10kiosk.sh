94abc29f39ca97708cfdb5f6f11f5d3d2060c6a4
Luigi Scagnet <luigi.scagnet@exorint.it>
2017-05-08 15:19:16 +0200

    BSP-574 Epad in Fastboot use Jmobile Libs.

--- files/mainos/etc/X11/Xsession.d/10kiosk.sh
+++ files/mainos/etc/X11/Xsession.d/10kiosk.sh
@@ -3,16 +3,21 @@
 echo "HMI: booting!" | logger
 
 export TMPDIR=/mnt/.psplash/
+# Source defaults.
+. /etc/default/rcS
 
-# starts EPAD service
-dbus-send --system --dest=com.exor.EPAD '/' com.exor.EPAD.ping
+if [ ! -z "$FASTBOOT" ] && [ -d "/mnt/data/hmi/$FASTBOOT/deploy" ] ; then
+	LD_LIBRARY_PATH=/mnt/data/hmi/$FASTBOOT/deploy EPAD &
+else
+    # starts EPAD service
+    dbus-send --system --dest=com.exor.EPAD '/' com.exor.EPAD.ping
+fi
 
 if [[ -e $TMPDIR/taptap ]] ; then
 	while ( (cat $TMPDIR/taptap | grep wait) > /dev/null 2>&1 ) ; do
 		echo "HMI: waiting tapttap" | logger
 		ping -c 1 -i 0.3 127.0.0.1 > /dev/null 2>&1;
 	done
-
 
 	if [[ -e $TMPDIR/taptap ]] ; then
 		echo "HMI: tap tap content: $(cat $TMPDIR/taptap)" | logger
