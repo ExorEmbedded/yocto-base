651818b2f17bbfc708fc993598211546e99b7728
Arrigo Zanette <zanettea@gmail.com>
2018-01-17 04:12:26 -0800

    #1001 run.sh patch script hardening (do only once and sync fs after it)

--- files/mainos/etc/X11/Xsession.d/79JMRuntime_fix.sh
+++ files/mainos/etc/X11/Xsession.d/79JMRuntime_fix.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
-   if [ -f "/mnt/data/hmi/qthmi/run.sh" ]; then
+
+if  grep JMobile /mnt/data/hmi/qthmi/run.sh > /dev/null 2>&1 ; then
    sed -i"" "/JMLauncher.app/s/JMobile/HMI Runtime/" /mnt/data/hmi/qthmi/run.sh
+   sync
 fi
