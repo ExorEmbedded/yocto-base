e41ef14f15d52189bbe19478c0a14eb4c43161ab
Steven Dorigotti <steven.dorigotti@exorint.it>
2018-03-12 16:56:13 +0100

    BSP-847 Fix screensaver restore condition by migrating also programs section

--- files/mainos/etc/migrations/to_1_999_469/post.sh
+++ files/mainos/etc/migrations/to_1_999_469/post.sh
@@ -7,4 +7,6 @@ SCREENSAVER_CONF="$PRESERVEDPATH/etc/X11/app-defaults/XScreenSaver"
 
 [ ! -e "${SCREENSAVER_CONF}" ] && exit 0
 
-sed -i 's/^mode: off/mode: blank/' "${SCREENSAVER_CONF}"
+sed -i -e 's|^mode: off|mode: blank|' \
+       -e  's|^programs: /usr/bin/xset dpms force off|programs: /usr/bin/xscreensaver-trigger|' \
+       "${SCREENSAVER_CONF}"
