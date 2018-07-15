ac6840b5b6b3530fb85d4bf65c9e541a15b4ed9d
Steven Dorigotti <steven.dorigotti@exorint.it>
2018-07-15 12:46:11 +0200

    BSP-983 Fix migration of wifi settings

--- files/mainos/etc/migrations/to_1_999_515/post.sh
+++ files/mainos/etc/migrations/to_1_999_515/post.sh
@@ -5,23 +5,24 @@ PRESERVEDPATH=$1
 
 SYSPARAMSCMD="/usr/bin/sys_params"
 
+# modules not loaded yet - detect presence of wifi based on presence of script
+[ -e /etc/init.d/networking_kmod-wifi-rs9113.sh ] || exit 0
+
 # applicable only if wifi0 autostart was enabled on previous BSP (System Settings implementation)
-iw dev | grep -q "Interface wifi0" || exit 0
-grep -q '"enabled": true' /etc/jmuconfig/network.json || exit 0
+grep -q '"enabled": true' $PRESERVEDPATH/etc/jmuconfig/network.json || exit 0
 
 # enable with new implementation (EPAD System Parameters)
-${SYSPARAMSCMD} -w network/wifi/nic/wifi0/autostart "true"
+$SYSPARAMSCMD -w network/wifi/nic/wifi0/autostart "true"
+$SYSPARAMSCMD -w network/wifi/autostartNics "wifi0"
 
 CHOSEN="/tmp/post.sh-chosen"
-grep -A 6 '"chosen":' /etc/jmuconfig/network.json > $CHOSEN
+grep -A 6 '"chosen":' $PRESERVEDPATH/etc/jmuconfig/network.json > $CHOSEN
 
-${SYSPARAMSCMD} -w network/wifi/nic/wifi0/net/ssid "`grep '"ssid":' $CHOSEN | cut -d '"' -f 4`"
-${SYSPARAMSCMD} -w network/wifi/nic/wifi0/net/bssid "`grep '"bssid":' $CHOSEN | cut -d '"' -f 4`"
-${SYSPARAMSCMD} -w network/wifi/nic/wifi0/net/security "`grep '"security":' $CHOSEN | cut -d '"' -f 4`"
-${SYSPARAMSCMD} -w network/wifi/nic/wifi0/net/psk "`grep '"psk":' $CHOSEN | cut -d '"' -f 4`"
-
-dbus-send --print-reply --system --dest=com.exor.EPAD "/ServiceManager" com.exor.EPAD.ServiceManager.command string:"wifi" string:"apply" string:""
+$SYSPARAMSCMD -w network/wifi/nic/wifi0/net/ssid "`grep '"ssid":' $CHOSEN | cut -d '"' -f 4`"
+$SYSPARAMSCMD -w network/wifi/nic/wifi0/net/bssid "`grep '"bssid":' $CHOSEN | cut -d '"' -f 4`"
+$SYSPARAMSCMD -w network/wifi/nic/wifi0/net/security "`grep '"security":' $CHOSEN | cut -d '"' -f 4`"
+$SYSPARAMSCMD -w network/wifi/nic/wifi0/net/psk "`grep '"psk":' $CHOSEN | cut -d '"' -f 4`"
 
 cp /etc/EPAD/system.ini $PRESERVEDPATH/etc/EPAD/
-rm /etc/jmuconfig/network.json
+rm $PRESERVEDPATH/etc/jmuconfig/network.json
 sync
